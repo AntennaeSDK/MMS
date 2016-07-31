ae4e1cd4457bbf4c80ae40bac1751f969dd4527f
S Nambi <snambi@gmail.com>
2016-07-31 09:12:34 -0700

    cleaned up .gitignore rules

--- mobile-server/src/main/resources/db/scripts/H2_SQL.sh
+++ mobile-server/src/main/resources/db/scripts/H2_SQL.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 
 # replace the path to H2 jar appropriately
 java -cp ~/.m2/repository/com/h2database/h2/1.4.187/h2-1.4.187.jar org.h2.tools.Shell -url "jdbc:h2:file:~/.mms/mms" -user sa -password "" -driver org.h2.Driver
